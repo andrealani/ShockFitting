4c15bc9ea2b99e9a3e1b730acb45b88c1c8d2277
valentinadeamicis <valentina.deamicis91@gmail.com>
2014-11-07 11:36:19 +0100

    VD:added CopyMaker obj for framework folder

---
+++ src/Framework/CopyMaker.cxx
@@ -0,0 +1,46 @@
+// Copyright (C) 2013 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Framework/CopyMaker.hh"
+#include "Framework/Log.hh"
+
+//--------------------------------------------------------------------------//
+
+using namespace std;
+using namespace SConfig;
+
+//--------------------------------------------------------------------------//
+
+namespace ShockFitting {
+
+//--------------------------------------------------------------------------//
+
+CopyMaker::CopyMaker(const std::string& objectName) :
+  BaseShockFitting(objectName)
+{
+}
+
+//--------------------------------------------------------------------------//
+
+CopyMaker::~CopyMaker()
+{
+}
+
+//--------------------------------------------------------------------------//
+
+void CopyMaker::configure(OptionMap& cmap, const std::string& prefix)
+{
+  LogToScreen(VERBOSE, "CopyMaker::configure() => start\n");
+
+  BaseShockFitting::configure(cmap, prefix);
+
+  LogToScreen(VERBOSE, "CopyMaker::configure() => end\n");
+}
+
+//--------------------------------------------------------------------------//
+
+} // namespace ShockFitting
+
---
+++ src/Framework/CopyMaker.hh
@@ -0,0 +1,59 @@
+// Copyright (C) 2014 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef ShockFitting_CopyMaker_hh
+#define ShockFitting_CopyMaker_hh
+
+//--------------------------------------------------------------------------//
+
+#include "Framework/BaseShockFitting.hh"
+#include "SConfig/SharedPtr.hh"
+
+//--------------------------------------------------------------------------//
+
+namespace ShockFitting {
+
+//--------------------------------------------------------------------------//
+
+/// This class defines a CopyMaker, whose task is to make copy
+/// of the mesh variables and arrays.
+
+class CopyMaker : public BaseShockFitting {
+public:
+
+  /// typedef needed by the self-registration mechanism
+  typedef SConfig::Provider<CopyMaker> PROVIDER;
+
+  /// Constructor
+  /// @param objectName the concrete class name
+  CopyMaker(const std::string& objectName);
+
+  /// Destructor
+  virtual ~CopyMaker();
+
+  /// Set up this object before its first use
+  virtual void setup() = 0;
+
+  /// Unset up this object after its last use
+  virtual void unsetup() = 0;
+
+  /// Configures the options for this object.
+  /// To be extended by derived classes.
+  /// @param args is the ConfigArgs with the arguments to be parsed.
+  virtual void configure(SConfig::OptionMap& cmap, const std::string& prefix);
+
+  /// Copy arrays
+  virtual void copy() = 0;
+
+};
+
+//--------------------------------------------------------------------------//
+
+} // namespace ShockFitting
+
+//--------------------------------------------------------------------------//
+
+#endif // ShockFitting_CopyMaker_hh
