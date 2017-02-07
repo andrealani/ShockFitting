e41d71884ef6c3c81ab1c3ddc58627a4d32627b8
Andrea Lani <andrea.lani@gmail.com>
2017-02-07 19:23:53 +0100

    AL: added more comments

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cmake .. -DMPI_HOME=/software/alternate/coolfluid/cf2/2013.9/openmpi -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_INSTALL_PREFIX=$PWD/../INSTALL -DSF_BUILD_Framework_API=ON # -DCGAL_HOME=... -DCGAL_INCLUDE_DIR=.../include
+cmake .. -DMPI_HOME=/software/alternate/coolfluid/cf2/2013.9/openmpi -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_INSTALL_PREFIX=$PWD/../INSTALL -DSF_BUILD_Framework_API=ON 
+# if CGAL has to be used, add the following to the previous line with right full paths
+#-DCGAL_HOME=... -DCGAL_INCLUDE_DIR=.../include -DCGAL_LIBRARIES=...dylib;...dylib;...dylib
