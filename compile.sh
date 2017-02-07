62b33461eb5c21987a9bd19754cc6d848a952a40
Andrea Lani <andrea.lani@gmail.com>
2017-02-07 19:07:13 +0100

    AL: added configuration variables and examples to use CGAL

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake .. -DMPI_HOME=/software/alternate/coolfluid/cf2/2013.9/openmpi -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_INSTALL_PREFIX=$PWD/../INSTALL -DSF_BUILD_Framework_API=ON
+cmake .. -DMPI_HOME=/software/alternate/coolfluid/cf2/2013.9/openmpi -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_INSTALL_PREFIX=$PWD/../INSTALL -DSF_BUILD_Framework_API=ON # -DCGAL_HOME=... -DCGAL_INCLUDE_DIR=.../include
